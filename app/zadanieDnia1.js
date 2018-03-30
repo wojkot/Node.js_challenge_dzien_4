55fa6ad1824f1c3c405ecec23f63903db974e614
wkot <wojciech.kot93@gmail.com>
2018-03-30 15:36:12 +0200

    Full solution

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
 //Twój kod
+
+var http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const browserInfo = req.headers['user-agent'];
+
+    res.setHeader( 'Content-Type', 'text/plain');
+    res.write(browserInfo);
+    res.end();
+}).listen(3000);
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
